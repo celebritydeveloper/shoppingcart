509345175409f89a2feb85b2d702b570a58bd77c
Celebrity <essiensaviour.a@gmail.com>
2020-03-24 22:38:21 +0100

    Added fetchfromstorage prototype

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
+
+
+// Initialize the HTMLUI 
+const cart = new Cart();
+const html = new HTMLUI();
+
 // Event Listners function
 
-const cart = new Cart();
-
 document.addEventListener("DOMContentLoaded", function() {
-    // Initialize the HTMLUI 
-        const html = new HTMLUI();
-        
-        
+    
+    html.fetchCourseFromStorage();
+
     // Get the courses
     const list = document.getElementById("courses-list");
     list.addEventListener("click", function(e) {
         if(e.target.classList.contains("add-to-cart")) {
             const course = e.target.parentElement.parentElement;
-            html.displayCourse(course);
+            cart.addToLocalStorage(course);
         }
     });
 });
@@ -20,7 +23,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function HTMLUI() {}
 
-HTMLUI.prototype.displayCourse = function(course) {
+function Cart() {}
+
+
+
+
+HTMLUI.prototype.fetchCourseFromStorage = function() {
+    const courses = JSON.parse(localStorage.getItem("courses"));
+
+    courses.forEach(course => {
+        // Insert the selected course into the HTML mockup
+        const shoppingCart = document.querySelector("#cart-content tbody");
+        const row = document.createElement('tr');
+        
+            row.innerHTML = `
+                <tr>
+                    <td><img src="${course.img}" width="200"></td>
+                    <td>${course.title}</td>
+                    <td>${course.price}</td>
+                    <td><a href="javascript:void(0)" class="remove">x</a></td>
+                </tr>
+            `;
+        shoppingCart.appendChild(row);
+        
+    });
+}
+
+
+
+
+
+Cart.prototype.addToLocalStorage = function(course) {
 
     const courseList = {
         img: course.querySelector("img").src,
@@ -28,29 +61,6 @@ HTMLUI.prototype.displayCourse = function(course) {
         price: course.querySelector(".price span").textContent,
         id: course.querySelector(".info-card a").getAttribute("data-id")
     }
-
-    // Insert the selected course into the HTML mockup
-        const shoppingCart = document.querySelector("#cart-content tbody");
-        const row = document.createElement('tr');
-            row.innerHTML = `
-                <tr>
-                    <td><img src="${courseList.img}" width="200"></td>
-                    <td>${courseList.title}</td>
-                    <td>${courseList.price}</td>
-                    <td><a href="javascript:void(0)" class="remove">x</a></td>
-                </tr>
-            `;
-        shoppingCart.appendChild(row);
-        cart.addToLocalStorage(courseList);
-}
-
-
-function Cart(courseList) {
-    this.courseList = courseList;
-}
-
-
-Cart.prototype.addToLocalStorage = function(courseList) {
     
     if(!JSON.parse(localStorage.getItem("courses"))) {
         const courses = [];
@@ -60,6 +70,10 @@ Cart.prototype.addToLocalStorage = function(courseList) {
         const courses = JSON.parse(localStorage.getItem("courses"));
         courses.push(courseList);
         localStorage.setItem("courses", JSON.stringify(courses));
-    } 
+    }
+}
+
+cart.prototype.removeCourseFromStorage = function() {
+    
 }
 
